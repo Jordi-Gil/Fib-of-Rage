19a75ac5eafc87d048367d01b66cfd3a386ed347
Jordi <jordigilgonzalez@hotmail.com>
2018-10-21 18:09:27 +0200

    ataques

--- Fib of Rage/Player.cpp
+++ Fib of Rage/Player.cpp
@@ -10,10 +10,16 @@
 #define JUMP_HEIGHT 96
 #define FALL_STEP 4
 
+#define WIDTH_PLAYER 90*2
+#define HEIGHT_PLAYER 120*2
+
+#define KEY_J 106
+#define KEY_K 107
+#define KEY_L 108
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, KICK_RIGHT, KICK_LEFT, PUNCH_RIGHT, PUNCH_LEFT
 };
 
 
@@ -22,8 +28,8 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	bJumping = false;
 	spritesheet.loadFromFile("Resources/Sprites/ryu.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	
-	sprite = Sprite::createSprite(glm::ivec2(2*90, 2*120), glm::vec2(0.05f, 0.5f), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	sprite = Sprite::createSprite(glm::ivec2(WIDTH_PLAYER, HEIGHT_PLAYER), glm::vec2(0.05f, 0.5f), &spritesheet, &shaderProgram);
+	sprite->setNumberAnimations(8);
 	
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
 	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.55f, 0.5f));
@@ -50,7 +56,28 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.10f, 0.0f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.15f, 0.0f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.20f, 0.0f));
-		
+
+	sprite->setAnimationSpeed(KICK_RIGHT, 12);
+	sprite->addKeyframe(KICK_RIGHT, glm::vec2(0.60f, 0.0f));
+	sprite->addKeyframe(KICK_RIGHT, glm::vec2(0.65f, 0.0f));
+	sprite->addKeyframe(KICK_RIGHT, glm::vec2(0.70f, 0.0f));
+
+	sprite->setAnimationSpeed(KICK_LEFT, 12);
+	sprite->addKeyframe(KICK_LEFT, glm::vec2(0.40f, 0.5f));
+	sprite->addKeyframe(KICK_LEFT, glm::vec2(0.35f, 0.5f));
+	sprite->addKeyframe(KICK_LEFT, glm::vec2(0.30f, 0.5f));
+
+	sprite->setAnimationSpeed(PUNCH_RIGHT, 12);
+	sprite->addKeyframe(PUNCH_RIGHT, glm::vec2(0.25f, 0.0f));
+	sprite->addKeyframe(PUNCH_RIGHT, glm::vec2(0.30f, 0.0f));
+	sprite->addKeyframe(PUNCH_RIGHT, glm::vec2(0.35f, 0.0f));
+
+	sprite->setAnimationSpeed(PUNCH_LEFT, 12);
+	sprite->addKeyframe(PUNCH_LEFT, glm::vec2(0.60f, 0.5f));
+	sprite->addKeyframe(PUNCH_LEFT, glm::vec2(0.65f, 0.5f));
+	sprite->addKeyframe(PUNCH_LEFT, glm::vec2(0.70f, 0.5f));
+
+
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
@@ -65,7 +92,7 @@ void Player::update(int deltaTime)
 		if(sprite->animation() != MOVE_LEFT)
 			sprite->changeAnimation(MOVE_LEFT);
 		posPlayer.x -= 2;
-		if(map->collisionMoveLeft(posPlayer, glm::ivec2(90*2,120*2))) // porque 32?
+		if(map->collisionMoveLeft(posPlayer, glm::ivec2(WIDTH_PLAYER, HEIGHT_PLAYER)))
 		{
 			posPlayer.x += 2;
 			sprite->changeAnimation(STAND_LEFT);
@@ -76,7 +103,7 @@ void Player::update(int deltaTime)
 		if(sprite->animation() != MOVE_RIGHT)
 			sprite->changeAnimation(MOVE_RIGHT);
 		posPlayer.x += 2;
-		if(map->collisionMoveRight(posPlayer, glm::ivec2(90*2, 120*2)))
+		if(map->collisionMoveRight(posPlayer, glm::ivec2(WIDTH_PLAYER, HEIGHT_PLAYER)))
 		{
 			posPlayer.x -= 2;
 			sprite->changeAnimation(STAND_RIGHT);
@@ -85,7 +112,7 @@ void Player::update(int deltaTime)
 	else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN))
 	{
 		posPlayer.y += 2;
-		if (map->collisionMoveDown(posPlayer, glm::ivec2(90 * 2, 120 * 2)))
+		if (map->collisionMoveDown(posPlayer, glm::ivec2(WIDTH_PLAYER, HEIGHT_PLAYER)))
 		{
 			posPlayer.y -= 2;
 		}
@@ -93,16 +120,28 @@ void Player::update(int deltaTime)
 	else if (Game::instance().getSpecialKey(GLUT_KEY_UP))
 	{
 		posPlayer.y -= 2;
-		if (map->collisionMoveUp(posPlayer, glm::ivec2(90 * 2, 120 * 2)))
+		if (map->collisionMoveUp(posPlayer, glm::ivec2(WIDTH_PLAYER, HEIGHT_PLAYER)))
 		{
 			posPlayer.y += 2;
 		}
 	}
+	else if (Game::instance().getKey(KEY_K)) {
+		if (sprite->animation() == STAND_LEFT || sprite->animation() == MOVE_LEFT)
+			sprite->changeAnimation(KICK_LEFT);
+		else if (sprite->animation() == STAND_RIGHT || sprite->animation() == MOVE_RIGHT)
+			sprite->changeAnimation(KICK_RIGHT);
+	}
+	else if (Game::instance().getKey(KEY_J)) {
+		if (sprite->animation() == STAND_LEFT || sprite->animation() == MOVE_LEFT)
+			sprite->changeAnimation(PUNCH_LEFT);
+		else if (sprite->animation() == STAND_RIGHT || sprite->animation() == MOVE_RIGHT)
+			sprite->changeAnimation(PUNCH_RIGHT);
+	}
 	else
 	{
-		if(sprite->animation() == MOVE_LEFT)
+		if(sprite->animation() == MOVE_LEFT || sprite->animation() == KICK_LEFT || sprite->animation() == PUNCH_LEFT)
 			sprite->changeAnimation(STAND_LEFT);
-		else if(sprite->animation() == MOVE_RIGHT)
+		else if(sprite->animation() == MOVE_RIGHT || sprite->animation() == KICK_RIGHT || sprite->animation() == PUNCH_RIGHT)
 			sprite->changeAnimation(STAND_RIGHT);
 	}
 	
