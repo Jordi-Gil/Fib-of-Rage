89987941c11b8540d433edff4b2e49b1966d07be
Jordi Gil <jordigilgonzalez@hotmail.com>
2018-10-26 15:54:42 +0200

    o
    :construction:

--- Fib of Rage/Game.h
+++ Fib of Rage/Game.h
@@ -48,6 +48,7 @@ public:
 private:
 	bool bPlay;                       // Continue to play game?
 	Scene scene;                      // Scene to render
+	Scene gameScenes[2];
 	bool keys[256], specialKeys[256]; // Store key states so that 
 	                                  // we can have access at any time
 
