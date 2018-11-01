b6836612f6ce3e96fa00b93c902b4c9bb852815e
muniategui <javier.de.muniategui@ludiumlab.com>
2018-11-01 18:28:42 +0100

    mapa 2

---
+++ Fib of Rage/Resources/LevelBackground/city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="city" tilewidth="1" tileheight="1" tilecount="998272" columns="1408">
+ <image source="../Sprites/SNES - Spider-Man and Venom Maximum Carnage - New York Streets 2.gif" width="1408" height="709"/>
+</tileset>
